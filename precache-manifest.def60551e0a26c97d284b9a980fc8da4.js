d428b39adf09c39d5a8efd9aa1dceacca69c5c62
hanhuayun <2256988633@qq.com>
2020-10-19 21:15:40 +0800

    ok

---
+++ precache-manifest.def60551e0a26c97d284b9a980fc8da4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30e47021070129761cb4dff2b7f095ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ca977225e240d7117c2",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "214506ff72584c63c921",
+    "url": "/static/css/main.44c6c6c7.chunk.css"
+  },
+  {
+    "revision": "4ca977225e240d7117c2",
+    "url": "/static/js/2.e6a4739a.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.e6a4739a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "214506ff72584c63c921",
+    "url": "/static/js/main.0197088f.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
